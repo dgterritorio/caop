a70d0cc712916108d9da516549a4b2d911792776
Alexander Bruy <alexander.bruy@gmail.com>
2023-10-26 10:13:23 +0300

    Portuguese strings

---
+++ qgis_caop_plugin/caop_tools/i18n/caoptools_pt.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CaopToolsPlugin</name>
+    <message>
+        <location filename="../plugin.py" line="64"/>
+        <source>CAOP Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="67"/>
+        <source>Split features</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="81"/>
+        <source>Editing reason</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SplitLineTool</name>
+    <message>
+        <location filename="../split_line_tool.py" line="83"/>
+        <source>Coordinate transform error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../split_line_tool.py" line="83"/>
+        <source>Cannot transform the point to the layers coordinate system</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../split_line_tool.py" line="103"/>
+        <source>CAOP features split</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../split_line_tool.py" line="129"/>
+        <source>Topological points from CAOP features split</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../split_line_tool.py" line="138"/>
+        <source>No features were split</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../split_line_tool.py" line="138"/>
+        <source>If there are selected features, the split tool only applies to those. If you would like to split all features under the split line, clear the selection.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../split_line_tool.py" line="166"/>
+        <source>No feature split done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../split_line_tool.py" line="148"/>
+        <source>Cut edges detected. Make sure the line splits features into multiple parts.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../split_line_tool.py" line="157"/>
+        <source>The geometry is invalid. Please repair before trying to split it.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../split_line_tool.py" line="166"/>
+        <source>An error occurred during splitting.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- qgis_caop_plugin/caop_tools/plugin.pro
+++ qgis_caop_plugin/caop_tools/plugin.pro
@@ -1,4 +1,6 @@
 SOURCES = __init__.py \
           plugin.py \
+          split_line_tool.py \
+          utils.py
 
 TRANSLATIONS = i18n/caoptools_pt.ts
