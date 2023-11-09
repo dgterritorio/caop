5504579d202fcfa5481f838fcb476deda9894f7d
Alexandre Neto <senhor.neto@gmail.com>
2023-11-09 15:30:05 +0000

    Tranduções para português

--- qgis_caop_plugin/caop_tools/i18n/caoptools_pt.ts
+++ qgis_caop_plugin/caop_tools/i18n/caoptools_pt.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="pt_PT" sourcelanguage="en">
 <context>
     <name>CaopToolsPlugin</name>
     <message>
         <location filename="../plugin.py" line="60"/>
         <source>CAOP Tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Ferramentas CAOP</translation>
     </message>
     <message>
         <location filename="../plugin.py" line="63"/>
         <source>Split features</source>
-        <translation type="unfinished"></translation>
+        <translation>Dividir Elementos</translation>
     </message>
     <message>
         <location filename="../plugin.py" line="79"/>
         <source>Editing reason (max. 255 characters)</source>
-        <translation type="unfinished"></translation>
+        <translation>Motivo de edição (max. 255 caracteres)</translation>
     </message>
 </context>
 <context>
@@ -24,52 +24,52 @@
     <message>
         <location filename="../split_line_tool.py" line="83"/>
         <source>Coordinate transform error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro de transformção de coordenadas</translation>
     </message>
     <message>
         <location filename="../split_line_tool.py" line="83"/>
         <source>Cannot transform the point to the layers coordinate system</source>
-        <translation type="unfinished"></translation>
+        <translation>Não foi possível converter o ponto para o sistema de coordenadas da camada</translation>
     </message>
     <message>
         <location filename="../split_line_tool.py" line="103"/>
         <source>CAOP features split</source>
-        <translation type="unfinished"></translation>
+        <translation>Dividir Elementos CAOP</translation>
     </message>
     <message>
         <location filename="../split_line_tool.py" line="129"/>
         <source>Topological points from CAOP features split</source>
-        <translation type="unfinished"></translation>
+        <translation>Pontos Topológicos do dividir elementos CAOP</translation>
     </message>
     <message>
         <location filename="../split_line_tool.py" line="138"/>
         <source>No features were split</source>
-        <translation type="unfinished"></translation>
+        <translation>Nenhum elemento foi dividido</translation>
     </message>
     <message>
         <location filename="../split_line_tool.py" line="138"/>
         <source>If there are selected features, the split tool only applies to those. If you would like to split all features under the split line, clear the selection.</source>
-        <translation type="unfinished"></translation>
+        <translation>Se existirem elementos selecionados, a ferramenta apenas incide nos mesmos. Se deseja dividir todos os elementos debaixo da linha de divisão, limpe a seleção.</translation>
     </message>
     <message>
         <location filename="../split_line_tool.py" line="166"/>
         <source>No feature split done</source>
-        <translation type="unfinished"></translation>
+        <translation>Não houve divisão de elementos</translation>
     </message>
     <message>
         <location filename="../split_line_tool.py" line="148"/>
         <source>Cut edges detected. Make sure the line splits features into multiple parts.</source>
-        <translation type="unfinished"></translation>
+        <translation>Corte nos extremos detectado. Garanta que a linha divide elementos em multiplas partes.</translation>
     </message>
     <message>
         <location filename="../split_line_tool.py" line="157"/>
         <source>The geometry is invalid. Please repair before trying to split it.</source>
-        <translation type="unfinished"></translation>
+        <translation>A geometria é inválida. Por favor corrija antes de tentar dividi-la.</translation>
     </message>
     <message>
         <location filename="../split_line_tool.py" line="166"/>
         <source>An error occurred during splitting.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ocorreu um erro durante a divisão.</translation>
     </message>
 </context>
 </TS>
