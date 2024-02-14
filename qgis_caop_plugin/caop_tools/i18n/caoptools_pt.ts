664d87eed5394b8f85c4e7d7d6c03071da603e7a
Alexander Bruy <alexander.bruy@gmail.com>
2024-02-14 16:41:22 +0200

    update strings

--- qgis_caop_plugin/caop_tools/i18n/caoptools_pt.ts
+++ qgis_caop_plugin/caop_tools/i18n/caoptools_pt.ts
@@ -4,19 +4,97 @@
 <context>
     <name>CaopToolsPlugin</name>
     <message>
-        <location filename="../plugin.py" line="60"/>
+        <location filename="../plugin.py" line="70"/>
         <source>CAOP Tools</source>
         <translation>Ferramentas CAOP</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="63"/>
+        <location filename="../plugin.py" line="73"/>
         <source>Split features</source>
         <translation>Dividir Elementos</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="79"/>
+        <location filename="../plugin.py" line="90"/>
         <source>Editing reason (max. 255 characters)</source>
         <translation>Motivo de edição (max. 255 caracteres)</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="99"/>
+        <source>Update master outputs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="111"/>
+        <source>Update validation layers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="123"/>
+        <source>Generate CAOP version</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GenerateCaopVersion</name>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="51"/>
+        <source>Generate CAOP version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="54"/>
+        <source>Layer management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="69"/>
+        <source>Database connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="74"/>
+        <source>Output schema name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="80"/>
+        <source>Continental</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="80"/>
+        <source>Madeira Autonomous Region</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="80"/>
+        <source>Autonomous Region of the Azores - Western Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="80"/>
+        <source>Autonomous Region of the Azores - Central and Eastern Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="89"/>
+        <source>Region to process</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="94"/>
+        <source>Timestamp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="108"/>
+        <source>Could not retrieve connection details for {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/generate_caop_version.py" line="129"/>
+        <source>Error executing database function: {}</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -70,6 +148,112 @@
         <location filename="../split_line_tool.py" line="166"/>
         <source>An error occurred during splitting.</source>
         <translation>Ocorreu um erro durante a divisão.</translation>
+    </message>
+</context>
+<context>
+    <name>UpdateMasterOutputs</name>
+    <message>
+        <location filename="../processing/algs/update_master_outputs.py" line="47"/>
+        <source>Update master outputs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_master_outputs.py" line="50"/>
+        <source>Layer management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_master_outputs.py" line="65"/>
+        <source>Database connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_master_outputs.py" line="71"/>
+        <source>Continental</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_master_outputs.py" line="71"/>
+        <source>Madeira Autonomous Region</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_master_outputs.py" line="71"/>
+        <source>Autonomous Region of the Azores - Western Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_master_outputs.py" line="71"/>
+        <source>Autonomous Region of the Azores - Central and Eastern Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_master_outputs.py" line="80"/>
+        <source>Region to process</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_master_outputs.py" line="94"/>
+        <source>Could not retrieve connection details for {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_master_outputs.py" line="116"/>
+        <source>Error executing database function: {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UpdateValidationLayers</name>
+    <message>
+        <location filename="../processing/algs/update_validation_layers.py" line="46"/>
+        <source>Update validation layers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_validation_layers.py" line="49"/>
+        <source>Layer management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_validation_layers.py" line="64"/>
+        <source>Database connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_validation_layers.py" line="70"/>
+        <source>Continental</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_validation_layers.py" line="70"/>
+        <source>Madeira Autonomous Region</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_validation_layers.py" line="70"/>
+        <source>Autonomous Region of the Azores - Western Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_validation_layers.py" line="70"/>
+        <source>Autonomous Region of the Azores - Central and Eastern Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_validation_layers.py" line="79"/>
+        <source>Region to process</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_validation_layers.py" line="93"/>
+        <source>Could not retrieve connection details for {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../processing/algs/update_validation_layers.py" line="102"/>
+        <source>Error executing database function: {}</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- qgis_caop_plugin/caop_tools/plugin.pro
+++ qgis_caop_plugin/caop_tools/plugin.pro
@@ -1,6 +1,10 @@
 SOURCES = __init__.py \
           plugin.py \
           split_line_tool.py \
-          utils.py
+          utils.py \
+          processing/provider.py \
+          processing/algs/generate_caop_version.py \
+          processing/algs/update_master_outputs.py \
+          processing/algs/update_validation_layers.py
 
 TRANSLATIONS = i18n/caoptools_pt.ts
