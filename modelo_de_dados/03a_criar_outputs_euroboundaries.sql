b2ecac031c4c2682ce098bb0123fae227b1c4e5a
Alexandre Neto <senhor.neto@gmail.com>
2024-03-11 22:43:28 +0000

    Corrigir caso agregacao de areas administrativas

--- modelo_de_dados/03a_criar_outputs_euroboundaries.sql
+++ modelo_de_dados/03a_criar_outputs_euroboundaries.sql
@@ -283,7 +283,7 @@ FROM base.raa_cen_ori_centroide_ea AS ce;
 
 DROP TABLE IF EXISTS master.ebm_a CASCADE;
 CREATE TABLE master.ebm_a as
-SELECT
+SELECT DISTINCT ON ("InspireId")
 	concat('_EG.EBM:AA.','PT', caa.shn_prefix, caa.entidade_administrativa) AS "InspireId",
 	'2022-12-31'::timestamp AS "beginLifeSpanVersion",
 	'PT' AS "ICC",
@@ -292,7 +292,8 @@ SELECT
 	p.geometria::geometry(multipolygon, 4258) AS geometria
 FROM euroboundaries.ebm_poligonos_finais AS p
 	LEFT JOIN euroboundaries.caop_areas_administrativas AS caa ON (st_intersects(st_pointonsurface(p.geometria),caa.geometria))
-	LEFT JOIN euroboundaries.ebm_centroides AS ce ON st_intersects(p.geometria, ce.geometria);
+	LEFT JOIN euroboundaries.ebm_centroides AS ce ON st_intersects(p.geometria, ce.geometria)
+ORDER BY "InspireId","TAA";
 
 CREATE INDEX ON master.ebm_a USING gist(geometria);
 
